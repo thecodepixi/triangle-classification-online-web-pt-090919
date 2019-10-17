5211a5260459486e1a420b3577d4fbe58fcd6d6f
Emily Harber <emily.a.harber@gmail.com>
2019-10-17 23:18:19 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
-class Triangle
-  # write code here
+require "pry" 
+class Triangle  
+  
+  attr_accessor :side_a, :side_b, :side_c, :type 
+  
+  def initialize(side_a, side_b,side_c) 
+    @side_a = side_a 
+    @side_b = side_b
+    @side_c = side_c 
+    
+    @type = self.kind 
+  end 
+  
+  def kind 
+    # triangles with sides that are 0 or negative are invalid 
+    if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 
+      raise TriangleError 
+    elsif @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a 
+      raise TriangleError 
+    elsif @side_a == @side_b && @side_b == @side_c 
+      return :equilateral 
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c 
+      return :isosceles 
+    elsif @side_a != @side_b && @side_b != @side_c && @side_a != @side_c
+      return :scalene 
+    end 
+  end 
+  
+  class TriangleError < StandardError
+    def message 
+      "Invalid Triangle"
+    end 
+  end 
+  
 end
