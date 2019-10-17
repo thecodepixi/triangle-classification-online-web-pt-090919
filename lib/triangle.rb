74f1e9aaee20f2bbdce133caf3270d54649780ab
Emily Harber <emily.a.harber@gmail.com>
2019-10-17 23:15:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,7 @@ class Triangle
     # triangles with sides that are 0 or negative are invalid 
     if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 
       raise TriangleError 
+    elsif 
     elsif @side_a == @side_b && @side_b == @side_c 
       return :equilateral 
     elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c 
